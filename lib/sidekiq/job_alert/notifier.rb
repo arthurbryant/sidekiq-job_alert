010ef42c7cf6c1c40fde30bfbf164c54d5903d7d
feng.zhang <cst.feng@gmail.com>
2019-09-30 21:20:42 +0900

    feat: fix send notifier

--- lib/sidekiq/job_alert/notifier.rb
+++ lib/sidekiq/job_alert/notifier.rb
@@ -6,34 +6,53 @@ require 'sidekiq/job_alert/queue'
 module Sidekiq
   module JobAlert
     class Notifier
-      def initialize
-        @slack_config ||= JSON.parse(::Rails.configuration.x.sidekiq_job_alert.to_json, object_class: OpenStruct)
+      def initialize(config_path)
+        @slack_config ||= YAML::load_file(config_path)
+        @message = ''
       end
 
       def call
-        # all jobs
-        cnt = ::Sidekiq::JobAlert::Queue.all_job_cnt
-        message = make_message(cnt)
-        @slack_notifier.ping(message)
+        @message += make_dead_job_message
+        @message += make_job_message('alert_total_waiting_jobs', 'all')
+        keys = @slack_config[:alert_each_waiting_job].keys
+        keys.delete(:message)
+        keys.each do |key|
+          @message += make_job_message('alert_each_waiting_job', key)
+        end
+
+        return if @message.empty?
+
+        @message += @slack_config[:sidekiq_url]
+        slack_notifier.ping(@message)
       end
 
       private
 
-      def make_message(job_counter)
+      def make_dead_job_message
+        cnt = Sidekiq::JobAlert::Queue.dead_job_cnt
+        cnt.positive? ? make_message('alert_dead_jobs', cnt) : ''
+      end
+
+      def make_job_message(type, queue_name)
+        cnt = Sidekiq::JobAlert::Queue.queue_job_cnt(queue_name)
+        limit = @slack_config[type.to_sym][queue_name.to_sym][:limit].to_i
+        cnt > limit ? make_message(type, cnt, queue_name.to_s) : ''
+      end
+
+      def make_message(type, job_counter, queue_name = nil)
         format(
-          @slack_config.alerting_sidekiq_all_waiting_jobs.message,
-          env: ::Rails.env,
+          @slack_config[type.to_sym][:message],
           job_counter: job_counter,
-          url: @slack_config.alerting_sidekiq_all_waiting_jobs.url,
-          )
+          queue_name: queue_name
+        )
       end
 
       def slack_notifier
-        ::Slack::Notifier.new(
-          @slack_config.webhook_url,
-          username: @slack_config.username,
-          channel: @slack_config.channel,
-          )
+        Slack::Notifier.new(
+          @slack_config[:webhook_url],
+          username: @slack_config[:username],
+          channel: @slack_config[:channel]
+        )
       end
     end
   end
--- lib/sidekiq/job_alert/queue.rb
+++ lib/sidekiq/job_alert/queue.rb
@@ -1,15 +1,25 @@
 # frozen_string_literal: true
 
-require 'sidekiq'
+require 'sidekiq/api'
 
 module Sidekiq
   module JobAlert
-    module Queue
-      def all_job_cnt
-        Sidekiq::Queue.all.sum(&:size)
-      end
+    class Queue
+      class << self
+        def all_job_cnt
+          return 0 if Sidekiq::Queue.all == []
 
-      module_function :all_job_cnt
+          Sidekiq::Queue.all.sum(&:size)
+        end
+
+        def dead_job_cnt
+          Sidekiq::DeadSet.new.size
+        end
+
+        def queue_job_cnt(queue_name)
+          Sidekiq::Queue.new(queue_name).size
+        end
+      end
     end
   end
 end
